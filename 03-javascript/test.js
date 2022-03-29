167ce66487fb3c1274bf25b9ae04707564a017fb
Fernando Mattioli <fernando.mattioli@facthus.edu.br>
2022-03-28 21:08:41 -0300

    Acrescenta o projeto
    Acrescenta os exercícios (JavaScript) e o projeto.

--- 03-javascript/test.js
+++ 03-javascript/test.js
@@ -15,3 +15,45 @@ function testeSoma() {
 function alteraTexto() {
     document.getElementById("saida").innerHTML = "Mensagem de teste!";
 }
+
+/*
+ * Exercício 01
+ */
+function dobro() {
+    var n = parseInt(document.getElementById("numero").value);
+    document.getElementById("saida").innerHTML = "Dobro = " + 2 * n;
+}
+
+function triplo() {
+    var n = parseInt(document.getElementById("numero").value);
+    document.getElementById("saida").innerHTML = "Triplo = " + 3 * n;
+}
+
+/*
+ * Exercício 02
+ */
+function parimpar() {
+    var n = parseInt(document.getElementById("numero").value);
+
+    if (n % 2 == 0) {
+        document.getElementById("saida").innerHTML = "O número " + n + " é par.";
+    } else {
+        document.getElementById("saida").innerHTML = "O número " + n + " é ímpar.";
+    }
+}
+
+/*
+ * Exercício 03
+ */
+function primo() {
+    var n = parseInt(document.getElementById("numero").value);
+
+    for (var i = 2; i <= (n - 1); i++) {
+        if (n % i == 0) {
+            document.getElementById("saida").innerHTML = "O número " + n + " não é primo.";
+            return;
+        }
+    }
+
+    document.getElementById("saida").innerHTML = "O número " + n + " é primo.";
+}
